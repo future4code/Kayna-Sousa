1d7f540b350f885a15650731fe4a409a0b0678da
KaynaSousa <89230829+KaynaSousa@users.noreply.github.com>
2021-09-28 22:13:46 -0300

    Não realizei todos as questões

---
+++ quinzena2/Loop/index.js
@@ -0,0 +1,157 @@
+
+
+//exercicio de Interpretação
+
+//exercicio 1
+
+/*let valor = 0
+for(let i = 0; i < 6; i++) {
+  valor += i
+}
+console.log(valor)*/
+
+//resposta: vai ser somado todos oa valores até o I ficar igual a 5.
+
+
+//exercicio 2
+
+/*const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+for (let numero of lista) {
+  if (numero > 18) {
+		console.log(numero)
+	}
+}*/
+
+// resposta: Será impressa todos os numeros que vem depois de 18.
+
+
+//exercicio 3
+
+/*const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
+let quantidadeAtual = 0
+while(quantidadeAtual < quantidadeTotal){
+  let linha = ""
+  for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
+    linha += "*"
+  }
+  console.log(linha)
+  quantidadeAtual++
+}*/
+
+
+//exercicio de escrita
+
+
+// Exercicio 1
+
+
+
+/*let pets = []
+const quantidadeDePets= Number (prompt("Quantos Pets você tem?"))
+if(quantidadeDePets === 0) 
+console.log("Que pena! Você podeadotar um pet!")
+else {for (i=0;i<quantidadeDePets;i++){
+    const nomes = prompt("Qual nome do seu Pet?")
+    pets.push(nomes)
+}
+}
+console.log(pets)*/
+ 
+
+// Exercicio 2
+
+
+const arrayOriginal=[80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+//a)
+/*const imprimiValorOriginal =  (numeros) =>{
+    let numeracao = [ ]
+for (let numero of numeros){
+    numeracao += numero + [ ]}
+    return numeracao}
+
+    const resultado = imprimiValorOriginal(arrayOriginal)
+   // console.log(resultado)*/
+
+//b)
+
+/*const imprimiValorDivididoPor10 =  (numeros) =>{
+    let numeracao = " "
+for (let numero of numeros){
+    numeracao += numero/10 + " "}
+    return numeracao}
+
+    const resultado = imprimiValorDivididoPor10(arrayOriginal)
+    console.log(resultado)*/
+
+    //c
+
+      /*const imprimiValoresPares =  (array) =>{
+      let novoArray = []
+    for (let i=0; i< i/2===0;i++){
+    if(array[i]%2==0)
+    novoArray [i] = array [i] + [ ]
+return novoArray
+    }
+    }
+    console.log(imprimiValoresPares(arrayOriginal))*/
+
+    // Resposta: Não consegui resolver.
+
+    //d)
+    // Resposta: Não consegui resolver.
+    //e)
+// Resposta: Não consegui resolver.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
